4629a83ac2f40d054b55f13a98a1f5f8a63434b0
yagmurcetintas <yagmurcetin@gmail.com>
2020-11-23 21:07:38 -0500

    added smaller board logic and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./styles.css";
 import Gameboard from "./components/gameboard";
-import Title from "./components/title";
+import Title from "./components/text";
+import InformationBoard from "./components/informationBoard";
 
 function App() {
   return (
     <div className="w-full h-screen bg-gray-100">
       <div className="flex flex-col w-full justify-center content-center">
         <Title
-          style="title text-black text-4xl mt-6"
+          customStyle="title text-black text-4xl mt-6"
           text="Nested Tic Tac Toe"
         />
         <Gameboard />
-        {/* <InformationBoard /> */}
+        <InformationBoard />
       </div>
     </div>
   );
--- src/components/game.js
+++ src/components/game.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "../styles.css";
 
 const Game = () => {
   const [squares, setSquares] = React.useState(Array(9).fill(null));
 
+  const nextValue = calculateNextValue(squares);
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner);
+
   function renderSquare(i) {
     return (
       <button
-        className="border border-white h-full w-full text-lg bold focus:outline-none focus:bg-pink-200 "
+        className="title border border-white h-full w-full text-lg bold flex-1 focus:outline-none focus:bg-pink-200 "
         onClick={() => selectSquare(i)}
       >
         {squares[i]}
@@ -15,22 +20,86 @@ const Game = () => {
   }
 
   function selectSquare(square) {
-    return;
+    if (winner || squares[square]) {
+      return;
+    }
+    const squaresCopy = [...squares];
+    squaresCopy[square] = nextValue;
+    setSquares(squaresCopy);
   }
 
-  return (
+  function calculateNextValue(squares) {
+    const xCount = squares.filter((item) => item === "X").length;
+    const oCount = squares.filter((item) => item === "O").length;
+    return xCount === oCount ? "X" : "O";
+  }
+
+  function calculateWinner(squares) {
+    const winningPositionsArray = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let condition of winningPositionsArray) {
+      const [a, b, c] = condition;
+      if (
+        squares[a] &&
+        squares[b] &&
+        squares[c] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function calculateStatus(winner) {
+    return winner ? winner : null;
+  }
+
+  return winner ? (
+    <div className="relative">
+      <span className="title text-6xl text-black absolute absolute-center">
+        {winner}
+      </span>
+      <div className="w-24 h-24 md:w-32 md:h-32 grid m-1 border border-white rounded-sm opacity-25">
+        <div className="flex bg-gray-200">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="flex bg-gray-200">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="flex bg-gray-200">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="w-24 h-24 md:w-32 md:h-32 grid m-1 border border-white rounded-sm">
-      <div className="flex bg-pink-100">
+      <div className="flex bg-gray-200">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className="flex bg-pink-100">
+      <div className="flex bg-gray-200">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className="flex bg-pink-100">
+      <div className="flex bg-gray-200">
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
